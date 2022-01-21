ff9d0a2e59210695847e7773a1a7a16a90658879
ANDRES-GRANDA_OKTANA <andres.granda@oktana.com>
2022-01-21 10:09:26 -0500

    Minor fixes

--- src/components/portfolioAdvisor/amountsTableRow.jsx
+++ src/components/portfolioAdvisor/amountsTableRow.jsx
@@ -11,9 +11,14 @@ export default function AmountsTableRow (props) {
     const inputDifference = !difference ? "" : difference > 0 ? "inputDifferencePositive" : "inputDifferenceNegative"
 
     return (
-        <tr key={1} style={{cursor:"pointer"}}>
+        <tr>
             <td>{type}</td>
-            <td><input autoComplete="chrome-off" name={amount.name} value={amount.value} onChange={(e)=>handleInput(e.target.name, e.target.value)}/></td>
+            <td><input
+                autoComplete="chrome-off"
+                name={amount.name}
+                value={amount.value}
+                onChange={(e)=>handleInput(e.target.name, e.target.value)}
+            /></td>
             <td><input value={newAmount} disabled/></td>
             <td><input value={difference} disabled className={inputDifference}/></td>
         </tr>
